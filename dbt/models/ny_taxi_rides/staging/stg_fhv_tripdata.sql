4d988efe597f09d590749468f6e756033f91ec80
Gimour <mouratosg@gmail.com>
2024-02-22 15:46:56 +0200

    final commits (#15)

---
+++ dbt/models/ny_taxi_rides/staging/stg_fhv_tripdata.sql
@@ -0,0 +1,31 @@
+with 
+
+source as (
+
+    select * from {{ source('staging', 'fhv_2019') }}
+
+),
+
+renamed as (
+
+    select
+        dispatching_base_num,
+        pickup_datetime,
+        dropoff_datetime,
+        pulocationid,
+        dolocationid,
+        sr_flag,
+        affiliated_base_number
+
+    from source
+
+)
+
+select * from renamed
+
+
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
